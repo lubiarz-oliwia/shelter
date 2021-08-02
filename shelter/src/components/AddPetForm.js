b47f6153c8898dd7418d85742443866b618307b9
oliwia lubiarz <lubiarzoliwia@gmail.com>
2021-08-02 13:31:28 +0200

    add form completed

--- shelter/src/components/AddPetForm.js
+++ shelter/src/components/AddPetForm.js
@@ -1,54 +1,113 @@
-import React, { useState } from 'react'
-import Nav from '../elements/Nav'
-import Button from '../elements/Button'
+import React from 'react';
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
+import { addAnimal, getAnimalsDetails } from '../actions/animals';
+import { useHistory } from "react-router-dom";
+
+const ValidationSchema = Yup.object().shape({
+    name: Yup.string()
+        .min(2, 'Imię zbyt krótkie.')
+        .max(50, 'Imię zbyt długie.')
+        .required('To pole jest wymagane.'),
+    sex: Yup.string()
+        .oneOf(['pies', 'suczka'])
+        .required('To pole jest wymagane.'),
+    age: Yup.number()
+        .max(15, 'Zbyt duza liczba')
+        .required('To pole jest wymagane.'),
+    size: Yup.string()
+        .oneOf(['maly', 'sredni', 'duzy'])
+        .required('To pole jest wymagane.'),
+    weight: Yup.number()
+        .required('To pole jest wymagane.'),
+    breed: Yup.string()
+        .min(2, 'Nazwa rasy jest zbyt krótka.')
+        .max(50, 'Nazwa rasy jest zbyt długa.')
+        .required('To pole jest wymagane.'),
+    description: Yup.string()
+        .max(50, 'Opis jest zbyt długi.')
+});
 
 function AddPetForm() {
-    const [form, setForm] = useState({ name: "", date: "", age: "", size: '', breed: '', description: '' })
-
-    //date will be added automatically
+    let history = useHistory();
 
     return (
-        <>
-            <Nav />
-            <div className="container">
-                <form>
-                    <h1>Dodaj zwierzę</h1>
-                    <div>
+        <div>
+            <h1>Dodaj zwierzę</h1>
+            <Formik
+                initialValues={{
+                    name: "",
+                    sex: "",
+                    age: "",
+                    size: "",
+                    weight: "",
+                    breed: "",
+                    description: "",
+                }}
+                validationSchema={ValidationSchema}
+                onSubmit={values => {
+                    const all_values = {
+                        ...values, add_date: new Date().toLocaleDateString('pl-PL', {
+                            day: 'numeric', month: 'short', year: 'numeric'
+                        }).replace(/ /g, '-')
+                    }
+                    addAnimal(all_values).then(() => {
+                        history.push('/stronaadmina');
+                        getAnimalsDetails(all_values);
+                    })
+                }}
+            >
+                {({ errors, touched }) => (
+                    <Form className='addpet_form'>
                         <label>Imię</label>
-                        <input type="text" name="name" value={form.name} />
-                    </div>
-                    <div>
-                        <label>Płeć:</label>
-                        <select>
-                            <option value="male">Suczka</option>
-                            <option value="female">Pies</option>
-                        </select>
-                    </div>
-                    <div>
-                        <label>Data przyjazdu:</label>
-                        <input type="date" name="date" value={form.date} />
-                    </div>
-                    <div>
-                        <label>Wiek:</label>
-                        <input type="number" name="age" value={form.age} />
-                    </div>
-                    <div>
-                        <label>Rozmiar:</label>
-                        <input type="number" name="age" value={form.age} />
-                    </div>
-                    <div>
-                        <label>Rasa:</label>
-                        <input type="text" name="breed" value={form.breed} />
-                    </div>
-                    <div>
-                        <label>Opis:</label>
-                        <textarea name="descritpion" value={form.description} />
-                    </div>
-                    <Button button_text={"Dodaj"} />
-                </form>
-            </div>
-        </>
-    )
+                        <Field name="name" label='name' />
+                        {errors.name && touched.name ? (
+                            <div>{errors.name}</div>
+                        ) : null}
+                        <label>Płeć</label>
+                        <div>
+                            <Field type="radio" name="sex" value="pies" />
+                            <label>pies</label>
+                            <Field type="radio" name="sex" value="suczka" />
+                            <label>suczka</label>
+                            {errors.sex && touched.sex ? (
+                                <div>{errors.sex}</div>
+                            ) : null}
+                        </div>
+                        <label>Wiek</label>
+                        <Field name="age" type="number" />
+                        {errors.age && touched.age ? <div>{errors.age}</div> : null}
+                        <label>Rozmiar</label>
+                        <Field name="size" as="select">
+                            <option value="wybierz">--wybierz--</option>
+                            <option value="maly">Mały</option>
+                            <option value="sredni">Średni</option>
+                            <option value="duzy">Duzy</option>
+                        </Field>
+                        {errors.size && touched.size ? (
+                            <div>{errors.size}</div>
+                        ) : null}
+                        <label>Waga</label>
+                        <Field name="weight" type='number' />
+                        {errors.weight && touched.weight ? (
+                            <div>{errors.weight}</div>
+                        ) : null}
+                        <label>Rasa</label>
+                        <Field name="breed" label='breed' />
+                        {errors.breed && touched.breed ? (
+                            <div>{errors.breed}</div>
+                        ) : null}
+                        <label>Dodatkowe informacje</label>
+                        <Field as='textarea' name="description" />
+                        {errors.description && touched.description ? (
+                            <div>{errors.description}</div>
+                        ) : null}
+                        <button type="submit">Submit</button>
+                    </Form>
+                )}
+            </Formik>
+        </div>
+    );
 }
 
-export default AddPetForm
+export default AddPetForm;
